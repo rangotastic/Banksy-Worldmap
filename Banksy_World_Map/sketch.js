c73f69d41f2209e55264ab5f4144c69b9ff1ba84
rangotastic <39835949+rangotastic@users.noreply.github.com>
2018-06-29 22:41:16 +0200

    Add files via upload

---
+++ Banksy_World_Map/sketch.js
@@ -0,0 +1,12 @@
+let map;
+function preload(){
+	map = loadImage("/src/Map.png")
+}
+function setup() {
+
+	createCanvas(windowWidth,windowWidth/16*9);
+}
+
+function draw() {
+image(map,0,0,width,height);
+}
