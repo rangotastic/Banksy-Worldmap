89abc98042cb5322ba2acaae4a6ab8618276d7d1
rangotastic <39835949+rangotastic@users.noreply.github.com>
2018-06-30 23:41:12 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,18 +12,19 @@ function setup() {
 
   createCanvas(windowWidth, windowWidth / 16 * 9);
   pix = width / 1600
-  london = new Location("London", pix * 741, pix * 297, 23, "\nCurrently there are \n23 artworks in the \nLondon area");
-  bristol = new Location("Bristol", pix * 726, pix * 303, 6, "\nCurrently there are \n6 artworks in the \nBristol area");
-  southHampton = new Location("South Hampton", pix * 732, pix * 304, 1, "\nCurrently there is \n1 artwork in the \nSouth Hampton area");
-  torquay = new Location("Torquay", pix * 722, pix * 307, 1, "\nCurrently there is \n1 artwork in the \nTorquay area");
-  paris = new Location("Paris", pix * 749, pix * 317, pix * 6, "\nCurrently there are \n6 artworks in the \nParis area");
-  calais = new Location("Calais", pix * 752, pix * 304, 2, "\nCurrently there are \n2 artworks in the \nCalais area");
-  newYork = new Location("New York", pix * 448, pix * 358, 19, "\nCurrently there are \n19 artworks in the \nNew York area");
-  sanFrancisco = new Location("San Francisco", pix * 248, pix * 381, 3, "\nCurrently there are \n3 artworks in the \nSan Francisco area");
-  losAngeles = new Location("Los Angeles", pix * 263, pix * 401, 2, "\nCurrently there are \n2 artworks in the \nLos Angeles area");
-  newOrleans = new Location("New Orleans", pix * 376, pix * 416, 4, "\nCurrently there are \n4 artworks in the \nNew Orleans area");
-  jerusalem = new Location("Jerusalem", pix * 884, pix * 399, 5, "\nCurrently there are \n5 artworks in the \nJerusalem area");
-  tallin = new Location("Tallin", pix * 836, pix * 255, 1, "\nCurrently there is \n1 artwork in the \nTallin area");
+
+  london = new Location("London", pix * 741, pix * 297, 23, "\nCurrently there are \n23 banksy pieces in the \nLondon area");
+  bristol = new Location("Bristol", pix * 726, pix * 303, 6, "\nCurrently there are \n6 banksy pieces in the \nBristol area");
+  southHampton = new Location("South Hampton", pix * 732, pix * 304, 1, "\nCurrently there is \n1 banksy piece in the \nSouth Hampton area");
+  torquay = new Location("Torquay", pix * 722, pix * 307, 1, "\nCurrently there is \n1 banksy piece in the \nTorquay area");
+  paris = new Location("Paris", pix * 749, pix * 317, pix * 6, "\nCurrently there are \n6 banksy pieces in the \nParis area");
+  calais = new Location("Calais", pix * 752, pix * 304, 2, "\nCurrently there are \n2 banksy pieces in the \nCalais area");
+  newYork = new Location("New York", pix * 448, pix * 358, 19, "\nCurrently there are \n19 banksy pieces in the \nNew York area");
+  sanFrancisco = new Location("San Francisco", pix * 248, pix * 381, 3, "\nCurrently there are \n3 banksy pieces in the \nSan Francisco area");
+  losAngeles = new Location("Los Angeles", pix * 263, pix * 401, 2, "\nCurrently there are \n2 banksy pieces in the \nLos Angeles area");
+  newOrleans = new Location("New Orleans", pix * 376, pix * 416, 4, "\nCurrently there are \n4 banksy pieces in the \nNew Orleans area");
+  jerusalem = new Location("Jerusalem", pix * 884, pix * 399, 5, "\nCurrently there are \n5 banksy pieces in the \nJerusalem area");
+  tallin = new Location("Tallin", pix * 836, pix * 255, 1, "\nCurrently there is \n1 banksy piece in the \nTallin area");
 }
 
 function draw() {
