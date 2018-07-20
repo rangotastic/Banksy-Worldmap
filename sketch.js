79da9637f6490c3fb120b12930abd2a466ce810d
rangotastic <39835949+rangotastic@users.noreply.github.com>
2018-07-20 08:29:22 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let popped;
 let pix;
 
 function preload() {
-  map = loadImage("src/map.png")
+  map = loadImage("src/map2.png")
 
 }
 
@@ -29,7 +29,6 @@ function setup() {
 
 function draw() {
   popped = true;
-  background(255);
   image(map, 0, 0, width, height);
   calais.show();
   calais.popUp();
